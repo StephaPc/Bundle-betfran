865eef0b27a32a79733860b28c24e3e83b06319b
Stephany Porras <sporras9720@gmail.com>
2022-11-21 19:33:57 -0600

    Updates

---
+++ assets/js/scripts-min.js
@@ -0,0 +1,78 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+var modal = document.getElementById("modal-login");
+var btn = document.getElementById("btnlogin");
+var span = document.getElementsByClassName("loginModal__close")[0];
+
+function openModal() {
+    console.log("open");
+    modal.style.display = "block";
+}
+function closeModal() {
+    modal.style.display = "none";
+}
+btn.addEventListener('click', openModal);
+span.addEventListener('click', closeModal);
+
+},{}],2:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var tnsSingle = exports.tnsSingle = function tnsSingle() {
+	var slider = tns({
+		container: '#tnsSingle',
+		items: 1,
+		slideBy: 1,
+		autoplay: true,
+		autoplayButtonOutput: false,
+		speed: 1000,
+		mode: 'gallery',
+		mouseDrag: true,
+		controlsText: ['<i class="fas fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>']
+	});
+};
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+var topNav = exports.topNav = function topNav() {
+	var myFunction = function myFunction() {
+		document.querySelector('.hamburger').addEventListener('click', function (e) {
+			e.preventDefault();
+			[].map.call(document.querySelectorAll('.hamburger'), function (el) {
+				el.classList.toggle('is-active');
+			});
+			[].map.call(document.querySelectorAll('.top-nav__menu'), function (el) {
+				el.classList.toggle('show-top-nav');
+			});
+		});
+	};
+	myFunction();
+};
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+var _topNav = require('./components/topNav');
+
+var _tnsSlider = require('./components/tns-slider');
+
+var _modalLogin = require('./components/modal-login');
+
+var _modalLogin2 = _interopRequireDefault(_modalLogin);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+(0, _topNav.topNav)();
+(0, _tnsSlider.tnsSingle)();
+_modalLogin2.default;
+
+},{"./components/modal-login":1,"./components/tns-slider":2,"./components/topNav":3}]},{},[4]);
+
+//# sourceMappingURL=scripts-min.js.map
